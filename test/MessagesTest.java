11e0744faf6c20426319daddd6a53a8af349973f
stolik 4, Piotr, Piotr Tomasz, Tomasz <kretes@gazeta.pl>
2012-05-15 14:20:40 +0200

    better test for proper query for not filled messages for given language

--- test/MessagesTest.java
+++ test/MessagesTest.java
@@ -5,11 +5,18 @@ import models.Message;
 import models.MessageKey;
 
 import org.fest.assertions.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
+import play.test.Fixtures;
 import play.test.UnitTest;
 
 public class MessagesTest extends UnitTest {
+
+	@Before
+	public void cleanDB() {
+		Fixtures.deleteDatabase();
+	}
 
 	@Test
 	public void messageCanBeStored() {
@@ -18,23 +25,27 @@ public class MessagesTest extends UnitTest {
 
 		new Message(key, en, "Welcome!").save();
 
-		Assertions
-				.assertThat(
-						Message.find("byMessageKeyAndLanguage", key, en)
-								.fetch()).onProperty("message")
+		Assertions.assertThat(Message.find("byMessageKeyAndLanguage", key, en).fetch()).onProperty("message")
 				.containsOnly("Welcome!");
 	}
 
 	@Test
 	public void canFindKeysWithNoMessageForLanguage() {
 		Language en = new Language("English").save();
-		MessageKey key = new MessageKey("welcome").save();
+		MessageKey keyForWhichMessageDoesNotExistsForEN = new MessageKey("unhandledKey").save();
+		MessageKey keyForWhichMessageExistsForEN = new MessageKey("handledKey").save();
 
-		List<Object> keys = MessageKey.find("select k from MessageKey k")
-				.fetch();
+		new Message(keyForWhichMessageExistsForEN, en, "Welcome!").save();
 
-		Assertions.assertThat(keys).onProperty("messageKey")
-				.containsOnly("welcome");
+		Language fr = new Language("French").save();
+		new Message(keyForWhichMessageExistsForEN, fr, "Welcome!").save();
+		new Message(keyForWhichMessageDoesNotExistsForEN, fr, "Welcome!").save();
+
+		List<Object> keys = MessageKey.find(
+				"select k from MessageKey k where k not in "
+						+ "(select message.messageKey from Message message where message.language = ?)", en).fetch();
+
+		Assertions.assertThat(keys).onProperty("messageKey").containsOnly("unhandledKey");
 	}
 
 }
